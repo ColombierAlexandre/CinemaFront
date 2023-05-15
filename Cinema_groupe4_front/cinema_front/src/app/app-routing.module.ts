f53560c37d5b6d9d39137c7c4fd17ef110347998
Samir31000 <kamalsamir@outlook.fr>
2023-05-15 09:43:26 +0200

    première branche

--- Cinema_groupe4_front/cinema_front/src/app/app-routing.module.ts
+++ Cinema_groupe4_front/cinema_front/src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { AfficherUnFilmComponent } from './composant/afficher-un-film/afficher-u
 import { CarouselComponent } from './composant/carousel/carousel.component';
 import { AfficherCinemaComponent } from './composant/afficher-cinema/afficher-cinema.component';
 import { ListeFilmComponent } from './composant/liste-film/liste-film.component';
+import { ValiderTicketComponent } from './composant/valider-ticket/valider-ticket.component';
 
 const routes: Routes = [
   {path : "acceul", component : AfficherUnFilmComponent},
   {path : "cinemas", component : AfficherCinemaComponent},
-  {path : "films", component : ListeFilmComponent}
+  {path:"listeFilms",component:ListeFilmComponent},
+  {path:"validerTicket",component:ValiderTicketComponent},
+  
+  {path : "films", redirectTo: '/listeFilms'},
+  {path:"",redirectTo: '/listeFilms',pathMatch:'full'}
 ];
 
 @NgModule({
